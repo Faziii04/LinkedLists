94e047e5cfd56f953aa603da7c4e900f3f4584bb
Faziii04 <santyzambrana@gmail.com>
2026-03-27 10:25:35 -0400

    f

--- 12-2-26WorkSameLetters/Vector.cs
+++ 12-2-26WorkSameLetters/Vector.cs
@@ -187,6 +187,8 @@ namespace _12_2_26WorkSameLetters
 
             for (int i = 0; i < n - 1; i++)
             {
+                bool swapped = false; 
+
                 for (int j = 0; j < n - i - 1; j++)
                 {
                     if (arr[j] > arr[j + 1])
@@ -194,8 +196,12 @@ namespace _12_2_26WorkSameLetters
                         int? temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
+
+                        swapped = true; 
                     }
                 }
+
+                if (!swapped) break;
             }
 
             Console.WriteLine("\nArray ordenado con BubbleSort V3 (Lazos fijos).");
